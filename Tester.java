d5df8156f88ae7072d6efcbfb354bf89f433ccba
Matt Kinloch <mjkinloch@loyola.edu>
2018-09-18 13:13:47 -0400

    added test cases for right triangle

--- Tester.java
+++ Tester.java
@@ -5,5 +5,11 @@ public class Tester
     // Example test case
     // Star s = new Star(Shape.Color.green, 5);
     // System.out.println("star = " + s);
+
+    Shape rt1 = new RightTriangle(Shape.Color.blue, 10, 10, "Right Triangle 1");
+    System.out.println("rt1: " + rt1);
+
+    Shape rt2 = new RightTriangle(Shape.Color.red, 20, 5, "Right Triangle 2");
+    System.out.println("rt2: " + rt2);
   }
 }
